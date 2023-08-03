2afdb9ea658a322edec75d6cb431f3f27611591b
Yemny <y.feliz1296@gmail.com>
2023-08-03 18:26:38 -0400

    no next btn, display in css, forloop for questions, check ans function, eventlist for choices

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
-var Intro = document.getElementsByClassName("intro");
+var Intro = document.getElementById("intro");
 var startButton = document.getElementById("startbtn");
-var Quiz = document.getElementsByClassName("Quiz");
+var Quiz = document.getElementById("Quiz");
 var timerEL = document.getElementById("Timer-count");
 var Questions = document.getElementById("Questions");
 var choice1 = document.getElementById("answer1");
@@ -9,17 +9,21 @@ var choice3 = document.getElementById("answer3");
 var choice4 = document.getElementById("answer4");
 
 var timeLeft = 180;
-QuestionIndex = 0;
+var QuestionIndex = 0;
 
-startButton.addEventListener("click", startTimer);
+startButton.addEventListener("click", function (){
+    Intro.style.display = "none";
+    Quiz.style.display = "block";
+    startTimer();
+    startQuiz();
+
+});
+
 function startTimer() {
-    questionIndex = 0;
-    timeLeft = 180;
-
     var timerInterval = setInterval(function () {
         timeLeft--;
         timerEL.textContent = timeLeft;
-        if (timeLeft === 0) {
+        if (timeLeft <= 0) {
             clearInterval(timerInterval);
 
         }
@@ -27,11 +31,9 @@ function startTimer() {
     }, 1000);
 }
 
-
-
 var QuizQuestions = [
     {
-        Quest1:"Which one is used to make the app interactive?",
+        Quest:"Which one is used to make the app interactive?",
         Options: [
             "1. CSS", 
             "2. HTML", 
@@ -43,17 +45,17 @@ var QuizQuestions = [
     },
 
     {
-        Quest2:"It can have a true or false value",
+        Quest:"It can have a true or false value",
         Options: [
             "1. Boolean", 
             "2. String", 
             "3. Number", 
             "4. Array", 
         ],
-        correctAns: "1.Boolean",
+        correctAns: "1. Boolean",
     },
     {
-        Quest3:"in the HTML the script.js file is usually located in",
+        Quest:"in the HTML the script.js file is usually located in",
         Options: [
             "1. The Title", 
             "2. The footer", 
@@ -63,7 +65,7 @@ var QuizQuestions = [
         correctAns: "3. At the end of the body",
     },
     {
-        Quest4:"An array may contain",
+        Quest:"An array may contain",
         Options: [
             "1. Numbers",
             "2. Strings", 
@@ -73,7 +75,7 @@ var QuizQuestions = [
         correctAns: "4. All of the above",
     },
     {
-        Quest5:"functions are created ",
+        Quest:"functions are created ",
         Options: [
             "1. funtion = nameFunction", 
             "2. function nameFunction()", 
@@ -85,6 +87,42 @@ var QuizQuestions = [
 
 ];
 
+function startQuiz() {
+    //startTimer()
+        Questions.textContent = QuizQuestions[QuestionIndex].Quest;
+        console.log(QuizQuestions[QuestionIndex].Options);
+           
+        for(var i = 0; i<QuizQuestions[QuestionIndex].Options.length; i++){
+            console.log(QuizQuestions[QuestionIndex].Options[i]);
+            document.getElementById("answer" + (i+1)).textContent = QuizQuestions[QuestionIndex].Options[i];
+
+        } 
+}
+
+function checkAns(event) {
+    console.log(event.target.textContent);
+    var clickAns = event.target.textContent;
+    var currentAns = QuizQuestions[QuestionIndex].correctAns;
+
+    if(clickAns == currentAns){
+        console.log("correct");
+        QuestionIndex++;
+        startQuiz();
+
+    }else {
+        timeLeft -= 10;
+        
+        QuestionIndex++;
+        console.log("next!");
+        startQuiz();
+        
+
+    }
 
 
+}
 
+choice1.addEventListener("click", checkAns);
+choice2.addEventListener("click", checkAns);
+choice3.addEventListener("click", checkAns);
+choice4.addEventListener("click", checkAns);
